2e6cb998b658b1291197b0d9e4bb649840e39c67
teamkotlin <muhammadramzan6254@gmail.com>
2023-07-04 07:11:05 +0500

    4 July

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,37 +13,64 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>
-    with SingleTickerProviderStateMixin {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   final HomeController _controller = HomeController();
 
   late AnimationController _batterAnimationController;
+  late AnimationController _tempAnimationController;
 
   late Animation<double> _animationBattery;
+  late Animation<double> _animationBatteryStatus;
+  late Animation<double> _animationCarShift;
 
   void setUpBatteryAnimation() {
     _batterAnimationController = AnimationController(
         vsync: this, duration: const Duration(milliseconds: 600));
     _animationBattery = CurvedAnimation(
         parent: _batterAnimationController, curve: const Interval(0.0, 0.5));
+    _animationBatteryStatus = CurvedAnimation(
+        parent: _batterAnimationController, curve: const Interval(0.6, 1));
   }
-@override
+
+  void setUpTempAnimation() {
+    _tempAnimationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 900));
+    _animationCarShift = CurvedAnimation(
+        parent: _tempAnimationController, curve: const Interval(0.2, 0.4));
+  }
+
+  @override
   void initState() {
     super.initState();
     setUpBatteryAnimation();
+    setUpTempAnimation();
   }
+
   @override
   void dispose() {
     super.dispose();
     _batterAnimationController.dispose();
+    _tempAnimationController.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: _controller,
+      animation: Listenable.merge(
+          [_controller, _batterAnimationController, _tempAnimationController]),
       builder: (BuildContext context, Widget? child) => Scaffold(
         bottomNavigationBar: TeslaBottomNavigation(
           onTap: (index) {
+            if (index == 1) {
+              _batterAnimationController.forward();
+            } else if (index != 1) {
+              _batterAnimationController.reverse();
+            }
+            if (index == 2) {
+              _tempAnimationController.forward();
+            } else if (_controller.selectedBottomTab == 2 && index != 2) {
+              _tempAnimationController.reverse(from: 0.4);
+            }
             _controller.onBottomNavigationTabChange(index);
           },
           selectedIndex: _controller.selectedBottomTab,
@@ -53,12 +80,21 @@ class _HomeScreenState extends State<HomeScreen>
             builder: (context, constraints) => Stack(
               alignment: Alignment.center,
               children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                      vertical: constraints.maxHeight * 0.1),
-                  child: SvgPicture.asset(
-                    'assets/icons/Car.svg',
-                    width: double.infinity,
+                SizedBox(
+                  width: constraints.maxWidth,
+                  height: constraints.maxHeight,
+                ),
+                Positioned(
+                  left: constraints.maxWidth / 2 * (_animationCarShift.value),
+                  width: constraints.maxWidth,
+                  height: constraints.maxHeight,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                        vertical: constraints.maxHeight * 0.1),
+                    child: SvgPicture.asset(
+                      'assets/icons/Car.svg',
+                      width: double.infinity,
+                    ),
                   ),
                 ),
                 AnimatedPositioned(
@@ -115,12 +151,58 @@ class _HomeScreenState extends State<HomeScreen>
                 ),
                 AnimatedOpacity(
                   duration: defaultDuration,
-                  opacity: _controller.selectedBottomTab == 1 ? 1 : 0,
+                  opacity: _animationBattery.value,
                   child: SvgPicture.asset(
                     'assets/icons/Battery.svg',
                     width: constraints.maxWidth * 0.45,
                   ),
-                )
+                ),
+                Positioned(
+                  top: 50 * (1 - _animationBatteryStatus.value),
+                  height: constraints.maxHeight,
+                  width: constraints.maxWidth,
+                  child: AnimatedOpacity(
+                    duration: defaultDuration,
+                    opacity: _animationBatteryStatus.value,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: constraints.maxHeight * 0.01,
+                        ),
+                        Text(
+                          '220 mi',
+                          style: Theme.of(context).textTheme.headline3,
+                        ),
+                        Text(
+                          '62 %',
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                        const Spacer(),
+                        Text('charging'.toUpperCase(),
+                            style: const TextStyle(fontSize: 20)),
+                        Text('18 min remaining'.toUpperCase(),
+                            style: const TextStyle(fontSize: 18)),
+                        SizedBox(
+                          height: constraints.maxHeight * 0.14,
+                        ),
+                        DefaultTextStyle(
+                          style: const TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: const [
+                                Text('22 mi/hr'),
+                                Text('232v'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
